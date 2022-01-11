c4242a9f9d7deec8479f6b1fcdc5aa972d83ce31
James <jimibue@gmail.com>
2022-01-11 09:38:21 -0800

    filepond front end setup

---
+++ client/src/components/UpdateUserImage.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+// Import React FilePond
+import { FilePond, registerPlugin } from 'react-filepond';
+
+// Import FilePond styles
+import 'filepond/dist/filepond.min.css';
+
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
+// Our app
+function UpdateUserImage() {
+    const [files, setFiles] = useState([]);
+
+    const handleUpdate = (files)=>{
+        console.log(files)
+        console.log(files[0])
+        console.log(files[0].file)
+        setFiles(files)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('handle submit clicked')
+        console.log(files)
+    }
+
+    return (
+        <div className="App">
+            <h1>only 1 photo and don't do api call on change</h1>
+            <form onSubmit={handleSubmit}>
+            <FilePond
+                files={files}
+                onupdatefiles={handleUpdate}
+                allowMultiple={false}
+                name="files"
+                labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+            />
+            <p>name</p>
+            <input />
+            <button type='submit'>update</button>
+            </form>
+            
+        </div>
+    );
+}
+
+export default UpdateUserImage
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import FilePondDemo from "../components/FilePondDemo";
+import UpdateUserImage from "../components/UpdateUserImage";
 import { AuthContext } from "../providers/AuthProvider";
 const Home = () => {
   const auth = useContext(AuthContext);
@@ -10,7 +11,10 @@ const Home = () => {
       <h1>Home!</h1>
       <code>{JSON.stringify(auth)}</code>
       <hr />
+      <h1>FilePondDemo 1</h1>
       <FilePondDemo />
+      <h1>FilePondDemo 2</h1>
+      <UpdateUserImage />
     </div>
   );
 };
